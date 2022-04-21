a310d2781962ff2c9ce8f7fb6780195803458122
jjak0b <jacopo.rimediotti@studio.unibo.it>
2022-04-22 01:05:26 +0200

    Add testing script

---
+++ test.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const fs = require("fs");
+const { edn2Obj, orcbrew2Foundry } = require("./index");
+
+const args = process.argv.slice(2);
+const inputFilename = args[0];
+
+var parentDir = path.dirname(inputFilename);
+
+const outputFilename = args.length > 1 ? args[1] : `${parentDir}/output.json`;
+
+if( !inputFilename ){
+	console.error("No edn/orcbrew file provided")
+	
+}
+else{
+	fs.readFile( inputFilename, "utf-8", (err, ednString) => {
+		if (err) {
+			throw err;
+		}
+		 
+		const orcbrewObj = edn2Obj(ednString);
+		const foundryObj = orcbrew2Foundry(orcbrewObj);
+	
+		const jsonStr = JSON.stringify(foundryObj, null, 2);
+		console.log("Foundry data:", jsonStr );
+	});
+}
