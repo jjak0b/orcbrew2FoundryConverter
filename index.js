dfd55625469dea77f096ea4e7cb988e070be3b31
jjak0b <jacopo.rimediotti@studio.unibo.it>
2022-04-23 01:08:58 +0200

    Add initial conversion; untested, just concettual since more must be investigated; Issue: group content items by data type per package (ignoring content title); or just add a package per data type for each content ?

--- index.js
+++ index.js
@@ -1,9 +1,63 @@
 // Thanks to god ( or thanks to its developer ) ... a library that works ...
 const edn = require('@yellowdig/cljs-tools').edn;
+const objectMapper = require('object-mapper');
+
+
+const mapSpell = require("./model/maps/spell");
+const mapClass = require("./model/maps/class");
+const mapSubClass = require("./model/maps/subclass");
+const mapFeat = require("./model/maps/feature");
+
+const spellDefaults = require("./model/defaults/spell");
+const classDefaults = require("./model/defaults/class");
+const subClassDefaults = require("./model/defaults/subclass");
+const featDefaults = require("./model/defaults/feature");
+
+const contentMap = {
+    "classes" : {
+        key: "items[]+",
+        transform: (obj) => toList(obj, mapClass, classDefaults)
+    },
+    "subclasses" : {
+        key: "items[]+",
+        transform: (obj) => toList(obj, mapSubClass, subClassDefaults)
+    },
+    "spells" : {
+        key: "items[]+",
+        transform: (obj) => toList(obj, mapSpell, spellDefaults)
+    },
+    "feats" : {
+        key: "items[]+",
+        transform: (obj) => toList(obj, mapFeat, featDefaults)
+    },
+    "invocations" : {
+        key: "items[]+",
+        transform: (obj) => toList(obj, mapFeat, featDefaults)
+    }
+}
+
+/**
+ * 
+ * @param {Object} src object to map
+ * @param {Object} map mapper obj
+ * @param {Object} defaults default dest values
+ * @returns 
+ */
+function toList(src, map, defaults) {
+    let items = [];
+    let item = null;
+    for (const key in src) {
+        item = objectMapper(src[key], Object.assign({}, defaults), map);
+        items.push(item);
+    }
+
+    return items;
+}
 
 /**
  * 
  * @param {String} ednString content in EDN format
+ * @returns {Object}
  */
 function edn2Obj( ednString ) {
 	const obj = edn.decode(ednString);
@@ -13,10 +67,35 @@ function edn2Obj( ednString ) {
 /**
  * Map and adapt orcbrew data to Foundry data
  * @param {Object} orcbrewObj 
+ * @returns {Object}
  */
 function orcbrew2Foundry( orcbrewObj ) {
-	// TODO
-	return orcbrewObj;
+	
+	let contentKeys = Object.keys(orcbrewObj);
+
+	// if keys are content-related (single orcbrew content)
+	if( contentKeys.some((key) => (key in contentMap)) ) {
+		return objectMapper(orcbrewObj, contentMap);
+	}
+	// else keys are about a map of orcbrew contents
+	else {
+		let contentModules = [];
+		let contentModule = null;
+		for (const key of contentKeys) {
+
+			let foundryPackage = objectMapper(orcbrewObj[ key ], contentMap );
+
+			contentModule = {
+				name: key,
+				data: foundryPackage
+			};
+
+			contentModules.push(contentModule);
+		}
+		return {
+			"mudules": contentModules
+		}
+	}
 }
 
 module.exports = {
